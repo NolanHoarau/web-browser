d235b28aa6a9d55118dd81fb117b04596e56a51d
NolanHoarau <nolanhoarau21@gmail.com>
2025-11-02 18:20:31 +0100

    web-browser

---
+++ Axolotl.py
@@ -0,0 +1,20 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import QtGui
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtCore import QUrl
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setWindowTitle("Axolotl")
+        self.setWindowIcon(QtGui.QIcon("logo.ico"))
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl("http://google.com"))
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
+
+app = QApplication(sys.argv)
+QApplication.setApplicationName('Axolotl')
+window = MainWindow()
+app.exec()
